85c693066a3570ca864b657cccd7d02f7aefc965
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-08-26 18:38:42 +0200

    .

--- add_sys_fun.sh
+++ add_sys_fun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 path=/usr/src/minix
-yes | cp -rf ./unistd.h root@localhost:/usr/include/
-yes | cp -rf ./unistd.h root@localhost:/usr/src/include/
-yes | cp -rf  ./callnr.h root@localhost:/usr/include/minix/
-yes | cp -rf ./callnr.h root@localhost:$path"/include/minix/"
-yes | cp -rf ./pm root@localhost:$path"servers" 
-yes | cp -rf ./check_if_ancestor.c root@localhost:/usr/src/lib/libc/misc
-yes | cp -rf ./Makefile.inc root@localhost:/usr/src/lib/libc/misc
+yes | cp -rf ./unistd.h /usr/include/
+yes | cp -rf ./unistd.h /usr/src/include/
+yes | cp -rf  ./callnr.h /usr/include/minix/
+yes | cp -rf ./callnr.h $path"/include/minix/"
+yes | cp -rf ./pm $path"servers" 
+yes | cp -rf ./check_if_ancestor.c /usr/src/lib/libc/misc
+yes | cp -rf ./Makefile.inc /usr/src/lib/libc/misc
