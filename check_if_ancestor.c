e9a142d831dda020507b2ad60a50cd0fec23b937
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-08-26 16:03:59 +0200

    needed files

---
+++ check_if_ancestor.c
@@ -0,0 +1,26 @@
+#include <lib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <minix/rs.h>
+
+int get_pm_endpt(endpoint_t *pt)
+{
+    return minix_rs_lookup("pm", pt);
+}
+
+uint8_t check_if_ancestor(pid_t pid1, pid_t pid2)
+{
+    endpoint_t pm_pt;
+    message m;
+    m.m1_i1 = (int)pid1;
+    m.m1_i2 = (int)pid2;
+
+    if (get_pm_endpt(&pm_pt) != 0)
+    {
+        errno = ENOSYS;
+        return -1;
+    }
+    return (_syscall(pm_pt, PM_ANCESTOR, &m));
+}
---
+++ pm/do_check_if_ancestor.c
@@ -0,0 +1,31 @@
+#include <lib.h>
+#include <stdio.h>
+#include <string.h>
+#include "pm.h"
+#include "mproc.h"
+#include "proto.h"
+
+int find_index(pid_t pid) {
+    int i;
+    for (i = 0; i < NR_PROCS; i++) {
+        if (mproc[i].mp_pid == pid) return i;
+    }
+    return -1;
+}
+
+uint8_t do_check_if_ancestor_recursion(pid_t pid1, pid_t pid2) {
+    int pid2_idx, pid2_parent_idx;
+    pid_t pid2_parent;
+    if ( (pid2_idx = find_index(pid2)) == -1) return 0; // check if process with pid2 exists
+    pid2_parent_idx = mproc[pid2_idx].mp_parent;
+    if ( (pid2_parent = mproc[pid2_parent_idx].mp_pid) == 0) return 0; // we have reached root
+    if (pid2_parent == pid1) return 1; // pid1 is an ancestor of pid2
+    return check_if_ancestor(pid1, pid2_parent);
+}
+
+uint8_t do_check_if_ancestor() {
+    pid_t pid1 = m_in.m1_i1, pid2 = m_in.m1_i2;
+    return do_check_if_ancestor_recursion(pid_t pid1, pid_t pid2);
+}
+
+
