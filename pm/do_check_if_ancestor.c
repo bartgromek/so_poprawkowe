c657ea85e7dbb069ce59a5c96a4e92b6f93114f6
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-08-28 19:01:50 +0200

    q

--- pm/do_check_if_ancestor.c
+++ pm/do_check_if_ancestor.c
@@ -23,7 +23,7 @@ int do_check_if_ancestor_recursion(pid_t pid1, pid_t pid2) {
     return check_if_ancestor(pid1, pid2_parent);
 }
 
-int do_check_if_ancestor() {
+int do_check_if_ancestor(message *) {
     pid_t pid1 = m_in.m1_i1, pid2 = m_in.m1_i2;
     return do_check_if_ancestor_recursion(pid1, pid2);
 }
