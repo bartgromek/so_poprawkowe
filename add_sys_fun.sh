bb40780130fa4105343a7dab1fceaafd91c6f9e8
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-08-28 21:56:05 +0200

    q

--- add_sys_fun.sh
+++ add_sys_fun.sh
@@ -4,6 +4,6 @@ yes | cp -rf ./unistd.h /usr/include/
 yes | cp -rf ./unistd.h /usr/src/include/
 yes | cp -rf  ./callnr.h /usr/include/minix/
 yes | cp -rf ./callnr.h $path"/include/minix/"
-yes | cp -rf ./pm $path"servers" 
+yes | cp -rf ./pm $path/"servers" 
 yes | cp -rf ./check_if_ancestor.c /usr/src/lib/libc/misc
 yes | cp -rf ./Makefile.inc /usr/src/lib/libc/misc
