2a24747b3ac1ac2adaa39911c4714130a2c5e3ca
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-08-28 19:28:49 +0200

    q

---
+++ test_ancestor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+int main () {
+    switch (pid = fork()) {
+        case -1:
+            syserr("Error in fork\n");
+            
+        case 0: /* proces potomny */
+            
+            return 0;
+            
+        default: /* proces macierzysty */
+            pid_t my_pid = getpid();
+            assert(check_if_ancestor(my_pid, pid) == 1);
+            assert(check_if_ancestor(my_pid, pid+1) == 0);
+            assert(check_if_ancestor(my_pid-1, pid) == 0);
+    }
+    return 0;
+}
