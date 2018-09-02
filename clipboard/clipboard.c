d1c62f6d1316f711ae4aacf7fb46929c4e05592c
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-09-02 11:39:23 +0200

    q

--- clipboard/clipboard.c
+++ clipboard/clipboard.c
@@ -188,13 +188,11 @@ static ssize_t clipboard_read(devminor_t UNUSED(minor), u64_t UNUSED(position),
     free(regs[id]);
     enQueue(q, id);
     captured_idx[id] = 0;
-    text[len] = '\0';
     /* Copy the requested part to the caller. */
-    if ((ret = sys_safecopyto(endpt, grant, 0, (vir_bytes) text, (size_t)10)) != OK){
+    if ((ret = sys_safecopyto(endpt, null, 0, (vir_bytes) text, len)) != OK){
         free(text);
         return ret;
     }
-    printf("text: %s\n", text);
     free(text);
     /* Return the number of bytes read. */
     return len;
--- simple_test_clipboard.c
+++ simple_test_clipboard.c
@@ -12,7 +12,7 @@ int main() {
 
     int size = (int)strlen(msg);
     int id = write(fd, msg, size);
-    size = read(fd, buf, size);
+    size = read(fd, buf, id);
     printf("size %d\n", size);
     buf[size] = '\0';
     printf("id: %d buf: %s\n", id, buf);
