a8840bac2e91f3ab638dee9e76fd78a00e719395
Bartosz Gromek <bartoszgromek@Bartoszs-MacBook-Pro.local>
2018-09-02 09:05:02 +0200

    q

--- simple_test_clipboard.c
+++ simple_test_clipboard.c
@@ -12,7 +12,7 @@ int main() {
 
     int size = (int)strlen(msg);
     int id = write(fd, msg, size);
-    size = read(fd, buf, 10);
+    size = read(fd, buf, id);
     printf("size %d\n", size);
     buf[size] = '\0';
     printf("id: %d buf: %s\n", id, buf);
